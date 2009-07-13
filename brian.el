9c3fd396f32b031000b12f5fa68c08af14c8bec3
Brian Clubb <bclubb@gmail.com>
2009-07-13 12:19:59 -0500

    adding autotest for emacs

--- brian.el
+++ brian.el
@@ -5,7 +5,7 @@
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-set-key "\C-c1" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
+;;(global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 (setq org-log-done t)
 (add-hook 'org-mode-hook 'turn-on-font-lock)
@@ -22,3 +22,10 @@
       '(("freenode" nickserv "coderdad" "password")))
 
 (require 'arduino-mode)
+(require 'rspec-mode)
+(require 'csharp-mode)
+(autoload 'autotest-switch "autotest" "doco" t)
+(autoload 'autotest "autotest" "doco" t)
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (define-key ruby-mode-map (kbd "C-c C-a") 'autotest-switch))
