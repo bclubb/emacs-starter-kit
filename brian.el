9930c2c3a2e4d0c23df5939398a60e59787aee0c
Brian Clubb <bclubb@gmail.com>
2009-07-21 03:41:52 -0500

    was causing a problem with org-mode when I would try to create custom TODO sequences

--- brian.el
+++ brian.el
@@ -5,6 +5,7 @@
 ;;(add-to-list 'load-path "~/.emacs.d/emacs-jabber")
 ;;(require 'jabber-autoloads)
 
+
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-set-key "\C-c1" 'org-store-link)
@@ -18,22 +19,6 @@
                              "~/Documents/org/recognition.org"
                              "~/Documents/org/warthog.org"))
 
-;;;; TODOs
-
-(setq org-todo-keywords
-           '((sequence "TODO(t)" "|" "DONE")
-             (sequence "REQUEST" "|" "SUBMITTED" "|" "RECEIVED" | "APPLIED")
-             (sequence "|" "CANCELED")))
-
-(setq org-todo-keyword-faces
-      '(("TODO" . (:foreground "red" :weight bold))
-  ("ACTION" . (:foreground "red" :weight bold))
-  ("DONE" . (:foreground "green" :weight bold))
-  ("CANCELED" . (:foreground "brown" :weight bold))
-  ("WAITING" . (:foreground "DarkOrchid1" :weight bold))
-  ("PROJECT" . (:foreground "blue" :weight bold))
-  ("FINISHED" . (:foreground "green" :weight bold))
-  ("STOPPED" . (:foreground "brown" :weight bold))))
 
 (setq compilation-scroll-output 1)
 ;;(add-hook 'org-mode-hook)
